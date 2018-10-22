9f8268d98425efc7b90c873894cd807794a22974
ivanrudn0303 <ivanrudn0303@gmail.com>
2018-10-22 14:55:38 +0300

    Create week3.h

---
+++ week3.h
@@ -0,0 +1,53 @@
+#ifndef STACK_H
+#define STACK_H
+#include <cstdint>
+
+#define ERROR_ALLOCATION 1
+#define ERROR_DATA 2
+#define ERROR_OVERFLOW 3
+#define ERROR_UNDERFLOW 4
+#define ERROR_POINTER 5
+
+
+#define STACK_DEBUG
+
+
+#ifdef STACK_DEBUG
+
+//hash
+uint64_t inline hash(uint64_t x)
+{
+	uint64_t a = 6364136223846793005;
+	uint64_t c = 1442695040888963407;
+	return (x * a + c);
+}
+//~hash
+
+using Stack = struct Stack
+{
+	uint64_t Hash;
+	uint32_t Size;
+	double *Data;
+	uint32_t Capacity;
+};
+
+uint64_t StackHash(const Stack*);
+int StackError(const Stack*);
+
+#else
+
+using Stack = struct Stack
+{
+	uint32_t Size;
+	double *Data;
+	uint32_t Capacity;
+};
+#endif
+
+int StackCreate(Stack*, uint32_t);
+int Push(Stack*, double);
+int Pop(Stack*, double*);
+bool inline Empty(const Stack*);
+int Clear(Stack*);
+int Erase(Stack*);
+#endif // !STACK_H
