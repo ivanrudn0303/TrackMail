f0e39c58d1a2ab286683bf45d8681faa0c2c8ada
ivanrudn0303 <ivanrudn0303@gmail.com>
2018-11-09 00:33:05 +0300

    add enum Command; Changed struct Mark

--- CPU/Compiler.h
+++ CPU/Compiler.h
@@ -9,16 +9,26 @@ typedef struct Mark
 {
 	std::string Str;
 	uint32_t Line;
+	std::vector<uint32_t> Paste;
 } Mark;
 
 #define COMMAND(name, num) int COMPILE_##name(const char*, const char*, char*, uint32_t*, std::vector<Mark>*);
 
 #include "CommandList.h"
-int COMPILE_RET(const char*, char*, uint32_t*);
+int COMPILE_EXIT(const char*, char*, uint32_t*);
+#undef COMMAND
 
 char* ReadData(const char*);
 char* BinaryCreate(const char*, uint32_t*);
 int WriteData(const char*, uint32_t, const char*);
 
+
+#define COMMAND(name, num) BYTE_##name = num,
+
+enum Command
+{
+	#include "CommandList.h"
+	BYTE_EXIT = 32
+};
 #undef COMMAND
 #endif // !COMPILER_H
