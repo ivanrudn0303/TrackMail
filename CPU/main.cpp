cdeb39fbd640195630f51a65367edefb420a8adb
ivanrudn0303 <ivanrudn0303@gmail.com>
2018-11-15 01:05:51 +0300

    Update main.cpp

--- CPU/main.cpp
+++ CPU/main.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include "Processor.h"
 #include "Compiler.h"
+#include "Disassembler.h"
 
 int main()
 {
 	printf("proc\n");
 	Processor Proc;
 	std::cout << ProcessorCreate(&Proc) << std::endl;
-	char* Text = ReadData("Factorial.txt");
+//	char* Text = ReadData("Factorial.txt");
+	char* Text = ReadData("SquareRoot.txt");
+//	char* Text = ReadData("Dis.txt");
 	uint32_t size;
 	char* Code = BinaryCreate(Text, &size);
-	WriteData(Code, size, "Factorial.o");
+	int re = Disassemble(Code,"dis.txt", size);
+//	WriteData(Code, size, "Factorial.o");
 	free(Code);
-	Code = ReadData("Factorial.o");
-	double data[] = { 1.5, 4.0 };
-	double ans;
-	double in;
-	std::cin >> in;
-	std::cout << ProcessorExec(&Proc, Code, 1, &in, &ans) << std::endl;
-	std::cout << ans << std::endl;
+	char* dis = ReadData("dis.txt");
+	Code = BinaryCreate(dis, &size);
+	double ans[3] = {NAN, NAN, NAN};
+	double in[3];
+	std::cin >> in[0] >> in[1] >> in[2];
+	std::cout << ProcessorExec(&Proc, Code, 3, in, ans) << std::endl;
+	std::cout <<"roots: "<< ans[0] << std::endl <<ans[1] << " " << ans[2] <<std::endl;
+	free(dis);
 	free(Text);
 	free(Code);
 	ProcessorDestruct(&Proc);
