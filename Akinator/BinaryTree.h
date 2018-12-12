0ca56530ce5a5158a2d548d0fb640e9b7bfe054c
ivanrudn0303 <ivanrudn0303@gmail.com>
2018-12-12 19:52:05 +0300

    Update BinaryTree.h

--- Akinator/BinaryTree.h
+++ Akinator/BinaryTree.h
@@ -3,16 +3,26 @@
 #pragma warning(disable : 4996)
 #include <iostream>
 
+#define DEBUG_BINARY_TREE
+
 using tdata = char;
 
 struct Tree
 {
 	Tree* Left;
 	Tree* Right;
+#ifdef DEBUG_BINARY_TREE
+	Tree* Top;
+#endif // !DEBUG_BINARY_TREE
 	tdata* Data;
 };
 
 int Akinator(Tree*);
-int Create(FILE*);
+int TreeCreate(Tree*, const char*);
+int TreeDestruct(Tree*);
+
+#ifdef DEBUG_BINARY_TREE
+int TreeError(const Tree*);
+#endif
 
 #endif // !BINARYTREE_H
