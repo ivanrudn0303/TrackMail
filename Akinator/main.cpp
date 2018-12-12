65238fcef83695844f64af1d84cb6dc754c10966
ivanrudn0303 <ivanrudn0303@gmail.com>
2018-12-12 19:51:44 +0300

    Update main.cpp

--- Akinator/main.cpp
+++ Akinator/main.cpp
@@ -1,21 +1,21 @@
 #pragma warning(disable : 4996)
 #include <iostream>
 #include <cstdint>
-#include <cstring>
 #include "BinaryTree.h"
 
 
 int main()
 {
-	Tree Tr;
-
-	Tr.Data = (tdata*)malloc(sizeof(tdata) * (strlen("You") + 1));
-	strcpy(Tr.Data, "You");
-	Tr.Left = nullptr;
-	Tr.Right = nullptr;
-	while (true)
-	{
-		Akinator(&Tr);
-	}
+	Tree *Tr = (Tree*)calloc(1, sizeof(Tree));
+	TreeCreate(Tr, "C:/Users/Иван/source/repos/ComputeMath/ComputeMath/text.txt");
+	int i = 0;
+	std::cout << TreeError(Tr) << std::endl;
+	TreeDestruct(Tr);
+//	Tr.Right->Right->Top = &Tr;
+//	std::cout << TreeError(&Tr) << std::endl;
+//	while (true)
+//	{
+//		Akinator(&Tr);
+//	}
 	return 0;
 }
