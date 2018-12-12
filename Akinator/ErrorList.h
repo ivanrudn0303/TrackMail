ebfb3f5480240fd6c9df3678185e089155a9c518
ivanrudn0303 <ivanrudn0303@gmail.com>
2018-12-12 19:52:46 +0300

    Create ErrorList.h

---
+++ Akinator/ErrorList.h
@@ -0,0 +1,10 @@
+#ifndef ERRORLIST_H
+#define ERRORLIST_H
+
+#define ERROR_CREATING 1
+#define ERROR_DATA_IN_FILE 2
+#define ERROR_POINTER 3
+#define ERROR_DATA 4
+#define ERROR_DATA_LEFT 5
+#define ERROR_DATA_RIGHT 6
+#endif // !ERRORLIST_H
